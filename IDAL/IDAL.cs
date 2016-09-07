58be2c6a61e764aab03d4e990781178b763209cd
Leonardo Paglionico <paglionico@seminabit.com>
2016-09-07 10:05:41 +0200

    Aggiunta Progetto Servizio

---
+++ IDAL/IDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    public interface IDAL
+    {
+        List<IDAL.VO.ChiamataVO> GetChiamateByRangeDate(DateTime begin, DateTime end);
+                
+        int UpdateChiamataByExtPk(IDAL.VO.ChiamataVO data, long extidid);
+        int UpdateChiamataByPk(IDAL.VO.ChiamataVO data, long idid);
+        int AddChiamata(IDAL.VO.ChiamataVO data);
+        int DeleteChiamata(string esamidid);
+    }
+}
