fddd382f231a3972fa599ba88445d8f867bbecd5
Leonardo Paglionico <paglionico@seminabit.com>
2016-09-07 10:15:41 +0200

    Modifica 2

---
+++ DAL/DBSQL.cs
@@ -0,0 +1,147 @@
+﻿using GeneralPurposeLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DBSQL
+    {
+        private static readonly log4net.ILog log =
+           log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static public DataTable ExecuteQuery(string connectionString, string sql)
+        {
+            Stopwatch tw = new Stopwatch();
+            tw.Start();
+
+            log.Info(string.Format("Setting up ..."));
+            //string connectionString = "";
+            DataTable dataTable = null;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                log.Info(string.Format("Opening Connection on '{0}' ...", connectionString));
+                connection.Open();
+                log.Info(string.Format("Query: {0}", sql));
+                log.Info(string.Format("Query execution starting ..."));
+                using (var reader = cmd.ExecuteReader())
+                {
+                    dataTable = new DataTable();
+                    dataTable.Load(reader);
+                }
+                log.Info(string.Format("Query executed! Result count: {0}", dataTable.Rows.Count));
+                connection.Close();
+                log.Info("Connection Closed!");
+            }
+
+            tw.Stop();
+
+            log.Info(string.Format("Completed! Elapsed time {0}", LibString.TimeSpanToTimeHmsms(tw.Elapsed)));
+
+            return dataTable;
+        }
+        static public DataTable ExecuteQueryWithParams(string connectionString, string sql, Dictionary<string, object> atts)
+        {
+            Stopwatch tw = new Stopwatch();
+            tw.Start();
+
+            log.Info(string.Format("Setting up ..."));
+            //string connectionString = "";
+            DataTable dataTable = null;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                foreach (KeyValuePair<string, object> entry in atts)
+                {
+                    cmd.Parameters.AddWithValue(entry.Key, entry.Value);
+                }
+                log.Info(string.Format("Opening Connection on '{0}' ...", connectionString));
+                connection.Open();
+                log.Info(string.Format("Query: {0}", LibString.SQLCommand2String(cmd)));
+                log.Info(string.Format("Query execution starting ..."));
+                using (var reader = cmd.ExecuteReader())
+                {
+                    dataTable = new DataTable();
+                    dataTable.Load(reader);
+                }
+                log.Info(string.Format("Query executed! Result count: {0}", dataTable.Rows.Count));
+                connection.Close();
+                log.Info("Connection Closed!");
+
+            }
+
+            tw.Stop();
+
+            log.Info(string.Format("Completed! Elapsed time {0}", LibString.TimeSpanToTimeHmsms(tw.Elapsed)));
+
+            return dataTable;
+        }
+
+        static public int ExecuteNonQuery(string connectionString, string sql)
+        {
+            Stopwatch tw = new Stopwatch();
+            tw.Start();
+
+            log.Info(string.Format("Setting up ..."));
+            int result = -1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                log.Info(string.Format("Opening Connection on '{0}' ...", connectionString));
+                connection.Open();
+                log.Info(string.Format("Query: {0}", sql));
+                log.Info(string.Format("Query execution starting ..."));
+                result = cmd.ExecuteNonQuery();
+                log.Info(string.Format("Query executed! Result count: {0}", result));
+                connection.Close();
+                log.Info("Connection Closed!");
+            }
+
+            tw.Stop();
+
+            log.Info(string.Format("Completed! Elapsed time {0}", LibString.TimeSpanToTimeHmsms(tw.Elapsed)));
+
+            return result;
+        }
+        static public int ExecuteNonQueryWithParams(string connectionString, string sql, Dictionary<string, object> atts)
+        {
+            Stopwatch tw = new Stopwatch();
+            tw.Start();
+
+            log.Info(string.Format("Setting up ..."));
+            int result = -1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                foreach (KeyValuePair<string, object> entry in atts)
+                {
+                    cmd.Parameters.AddWithValue(entry.Key, entry.Value);
+                }
+                log.Info(string.Format("Opening Connection on '{0}' ...", connectionString));
+                connection.Open();
+                log.Info(string.Format("Query: {0}", LibString.SQLCommand2String(cmd)));
+                log.Info(string.Format("Query execution starting ..."));
+                result = cmd.ExecuteNonQuery();
+                log.Info(string.Format("Query executed! Result count: {0}", result));
+                connection.Close();
+                log.Info("Connection Closed!");
+            }
+
+            tw.Stop();
+
+            log.Info(string.Format("Completed! Elapsed time {0}", LibString.TimeSpanToTimeHmsms(tw.Elapsed)));
+
+            return result;
+        }
+    }
+}
--- IDAL/IDAL.cs
+++ IDAL/IDAL.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL.VO;
 
 namespace IDAL
 {
     public interface IDAL
     {
-        List<IDAL.VO.ChiamataVO> GetChiamateByRangeDate(DateTime begin, DateTime end);
+        List<ChiamataVO> GetChiamateByRangeDate(DateTime begin, DateTime end);
                 
-        int UpdateChiamataByExtPk(IDAL.VO.ChiamataVO data, long extidid);
-        int UpdateChiamataByPk(IDAL.VO.ChiamataVO data, long idid);
-        int AddChiamata(IDAL.VO.ChiamataVO data);
+        int UpdateChiamataByExtPk(ChiamataVO data, long extidid);
+        int UpdateChiamataByPk(ChiamataVO data, long idid);
+        int AddChiamata(ChiamataVO data);
         int DeleteChiamata(string esamidid);
     }
 }
